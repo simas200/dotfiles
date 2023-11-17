12ef231c324ec31ddc6f3380d229fc2680819f0f
simas200 <siminskas@gmail.com>
2023-11-17 10:51:30 +0200

    herbs and polybar added

---
+++ herbstluftwm/colors.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env bash
+
+hc_color_1 = '#33162D'
+hc_color_2 = '#C34956'
+hc_color_3 = '#91728C'
+hc_color_4 = '#8C8C8C'
+hc_color_5 = '#D86967'
+hc_color_6 = '#130E21'
+hc_color_7 = '#1E0D17'
+hc_color_8 = '#231635'
+hc_color_9 = '#040304'
